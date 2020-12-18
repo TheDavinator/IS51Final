4c978df6c8800be309cc520be3511eba43457497
TheDavinator <70241745+TheDavinator@users.noreply.github.com>
2020-12-17 22:39:09 -0800

    add exam

---
+++ final.py
@@ -0,0 +1,23 @@
+def convert_list() :
+    path = "/Users/Dell/Documents/College Courses/IS-51/Final/Final.txt"
+    with open(path, "r") as filename:
+        scores = [[float(i) for i in scores.strip().split(',')] for scores in filename]
+        return [sum(i)/len(i) for i in scores]
+
+
+def main() :
+    counter = 0
+    scores = convert_list()
+    print("The scores for this class are:", scores)
+    avg = sum(scores) / len(scores)
+    for i in scores:
+        if float(i) > avg:
+            counter += 1
+            percent_avg = counter/len(scores)
+    print("The class average is:", avg)
+    print("The number of students is:", len(scores))
+    print("The percentage of the class above the average is:", percent_avg, "%")
+
+
+
+main()
