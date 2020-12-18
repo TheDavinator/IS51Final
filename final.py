8776227e8bb8322b4f962d61130767929025c432
TheDavinator <70241745+TheDavinator@users.noreply.github.com>
2020-12-17 22:30:05 -0800

    add pseudo

---
+++ final.py
@@ -0,0 +1,17 @@
+"""
+def convert_list
+path = path of text file
+with open(path ,"r") as filename
+    scorces = all the scores in the text file
+    return list of scores
+
+def main
+counter for later
+scores = covert_list to call function
+print("Theses are the student scores" , scores)
+avg = sum(scores list) / len (scores list)
+for i in scores:
+    if float(i) > avg:
+        counter add 1
+        %avg = counter / len(scorces)
+"""
